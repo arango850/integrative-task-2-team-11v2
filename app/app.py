ec9d1d5d08393a7db96fe2697b258a3521171550
Andres Arango <arango850@gmail.com>
2024-06-03 01:40:14 -0500

    Advances in flask

--- app/app.py
+++ app/app.py
@@ -1,16 +1,25 @@
-from flask import Flask
-app =Flask(__name__)
+from flask import Flask, request, render_template
 
+app = Flask(__name__)
 
-@app.route('/chat', methods=['POST'])
+# Importar la lógica del chatbot
+from mi_chatbot import Chatbot, Message
+
+# Crear una instancia del chatbot
+chatbot = Chatbot()
+
+@app.route('/chat', methods=['GET', 'POST'])
 def chat():
-    # Aquí se manejarán las solicitudes relacionadas con el chat
-    return "Hola desde el servidor!"
-
-@app.route('/')
-def index():
-    return "HolaMundo"
-
+     if request.method == 'POST':
+        data = request.json
+        message_content = data['message']
+    # Declarar el mensaje del usuario y ejecutar el chatbot
+        chatbot.reset()
+        chatbot.declare(Message(content=message_content))
+        chatbot.run()
+        return "Mensaje del usuario procesado por el chatbot"
+     else:
+         return render_template('basic.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
